92f39cf07c09c973da477ba31c05e7686595b104
Krushna <krushnahandinag02@gmail.com>
2023-12-26 09:36:48 +0000

    Cash Withdraws

---
+++ src/components/DenominationItem/index.js
@@ -0,0 +1,26 @@
+// Write your code here
+// Write your code here
+import './index.css'
+
+const DenominationItem = props => {
+  const {denominationsDetails, updateBalance} = props
+  const {value} = denominationsDetails
+
+  const onClickDenomination = () => {
+    updateBalance(value)
+  }
+
+  return (
+    <li className="denomination-item">
+      <button
+        type="button"
+        className="denomination-button"
+        onClick={onClickDenomination}
+      >
+        {value}
+      </button>
+    </li>
+  )
+}
+
+export default DenominationItem
